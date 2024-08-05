58c7dbd9ac2518b95893fea340ddd5107d8ceac7
sandeepkrpalit <sandeepkrpalit@gmail.com>
2024-08-06 00:50:43 +0530

    updated requirements.txt, setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Project",
+    version = "0.0.0",
+    author = "Sandeep Kumar Palit",
+    author_email="sandeepkrpalit@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
